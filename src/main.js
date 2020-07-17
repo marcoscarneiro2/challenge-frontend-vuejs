1ac2795b8a2ae822f99134820ac7fe67b6b71511
Hallison Melo <hallison.pm@gmail.com>
2020-07-17 14:56:39 -0300

    Initial Carousel

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import VueCarousel from "vue-carousel";
+Vue.use(VueCarousel);
 
-import '@/assets/scss/index.scss'
+import "@/assets/scss/index.scss";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,18 +1,59 @@
 <template>
-  <div>movies</div>
+  <div>
+    <carousel
+      v-if="medias.length > 0"
+      :perPage="3"
+      :autoplay="true"
+      :autoplayTimeout="5000"
+    >
+      <slide v-for="(media, index) in medias" :key="index">
+        <img :src="media.poster_path" :alt="media.title" class="c-img poster" />
+      </slide>
+    </carousel>
+  </div>
 </template>
 
 <script>
+import { Carousel, Slide } from "vue-carousel";
 import mediaService from "@/service/media";
 export default {
   name: "Movies",
+
+  components: {
+    Carousel,
+    Slide
+  },
   data() {
     return {
       medias: []
     };
   },
+  methods: {
+    async getAllMedia() {
+      const { data, status } = await mediaService.getAllMedia();
+      if (status == 200) this.medias = data;
+    }
+  },
   created() {
-    mediaService.getAllMedia();
+    this.getAllMedia();
   }
 };
 </script>
+<style lang="scss" scoped>
+.VueCarousel-slide {
+  position: relative;
+  background: transparent;
+  color: #fff;
+  font-family: Arial;
+  font-size: 24px;
+  text-align: center;
+  min-height: 100px;
+}
+
+.label {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
