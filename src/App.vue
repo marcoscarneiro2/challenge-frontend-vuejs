172cbfcf0f6edea6bbb07d8d21eb5bf6efa29e75
Anderson Martins <atmmoreira.rj@gmail.com>
2020-07-18 20:46:32 -0300

    add: teste intercase

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bgMovies">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+  body { background-color: #04203f !important; z-index: 0;}
+
+  .bgMovies { background:  url('../src/assets/img/bg.jpg') no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; height: 100vh; z-index: 1;}
+
+  .alignCenter { padding-top: 35%; }
+</style>
